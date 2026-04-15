088b15078d4a7d2e04d69bbd56eb591ded479901
0x01sky <sky@git.com>
2026-04-15 14:09:42 +0100

    Updates to my dotfiles

--- install.sh
+++ install.sh
@@ -27,5 +27,11 @@ cp -r Wallpapers/. "$HOME/Pictures/Wallpapers/"
 mkdir -p "$CONFIG"
 cp starship.toml "$CONFIG/"
 
+cd ~/Downloads 
+
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip"
+fc-cache -fv
+
 echo "[+] Your Dotfiles are installed, enjoy :-) !"
 echo "[+] Adding more stuffs soon :-)"
