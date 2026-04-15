b31d32f152972e093fb11e9468ef80913c559f69
0x01sky <sky@git.com>
2026-04-15 14:14:06 +0100

    Updates to my dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# This Copies the config files to your config directory
+
 set -e
 
 CONFIG="$HOME/.config"
@@ -27,10 +30,15 @@ cp -r Wallpapers/. "$HOME/Pictures/Wallpapers/"
 mkdir -p "$CONFIG"
 cp starship.toml "$CONFIG/"
 
-cd ~/Downloads 
+# Fonts Installation
 
+cd ~/Downloads 
+mkdir ~/.local/share/fonts
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip"
+unzip JetBrainsMono.zip -d JetBrainsMono && unzip FiraCode.zip -d FiraCode
+cd JetBrainsMono && mv *.ttf ~/.local/share/fonts/
+cd .. && cd FiraCode && mv *.ttf ~/.local/share/fonts/
 fc-cache -fv
 
 echo "[+] Your Dotfiles are installed, enjoy :-) !"
